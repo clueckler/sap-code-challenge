6deae971d1c768f023a877bfeffba7d143004ff2
Christoph Lückler <c.lueckler@netconomy.net>
2018-02-25 14:43:49 +0100

    Update storybaord

--- Offline/ProductListViewController.swift
+++ Offline/ProductListViewController.swift
@@ -7,10 +7,32 @@
 //
 
 import UIKit
+import SAPFiori
 
 class ProductListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: Init
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.customInit()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.customInit()
+    }
+    
+    private func customInit() {
+        // Configure tabBarItem
+        self.tabBarItem = UITabBarItem(title: "Products",
+                                       image: FUIIconLibrary.system.library.withRenderingMode(.alwaysTemplate),
+                                       tag: 0)
+    }
+    
+    // MARK: View life cylce
     
     override func viewDidLoad() {
         super.viewDidLoad()

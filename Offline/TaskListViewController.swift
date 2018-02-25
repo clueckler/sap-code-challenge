2eb936d40150e1f88258fa4dd3eb2b0a9334807b
Christoph Lückler <c.lueckler@netconomy.net>
2018-02-25 14:28:30 +0100

    TaskList implementation

--- Offline/TaskListViewController.swift
+++ Offline/TaskListViewController.swift
@@ -7,29 +7,111 @@
 //
 
 import UIKit
+import MapKit
+import SAPFiori
+
+struct DummyData {
+    
+    var title: String
+    var coordinate: CLLocationCoordinate2D
+}
 
 class TaskListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var tasks = [DummyData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.register(FUIObjectTableViewCell.self, forCellReuseIdentifier: FUIObjectTableViewCell.reuseIdentifier)
+        self.mapView.register(TaskAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
 
-        // Do any additional setup after loading the view.
+        self.loadDataForTableView()
+        self.loadDataForMapView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate
+extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FUIObjectTableViewCell.reuseIdentifier,
+                                                 for: indexPath) as! FUIObjectTableViewCell
+        
+        let task = self.tasks[indexPath.row]
+        cell.headlineText = task.title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+}
 
+extension TaskListViewController {
+    
+    private func loadDataForTableView() {
+        var data = DummyData(title: "fakeData", coordinate: CLLocationCoordinate2D(latitude: 41.395815, longitude: 2.173050))
+        self.tasks.append(data)
+    }
+    
+    private func loadDataForMapView() {
+        let annotations = TaskAnnotation.wrap(self.tasks)
+        if annotations.count > 0 {
+            self.mapView.addAnnotations(annotations)
+            self.mapView.showAnnotations(annotations, animated: true)
+        }
+    }
+    
+}
+
+class TaskAnnotation: MKPointAnnotation {
+    
+    // MARK: Properties
+    
+    let task: DummyData
+    
+    // MARK: Init
+    
+    init(_ task: DummyData) {
+        self.task = task
+        super.init()
+        
+        self.title = task.title
+        self.coordinate = task.coordinate
+    }
+    
+    static func wrap(_ tasks: [DummyData]) -> [TaskAnnotation] {
+        var retVal = [TaskAnnotation]()
+        for task in tasks {
+            retVal.append(TaskAnnotation(task))
+        }
+        return retVal
+    }
+    
+}
+
+class TaskAnnotationView: FUIMarkerAnnotationView {
+    
+    override var annotation: MKAnnotation? {
+        willSet {
+            self.displayPriority = .defaultLow
+        }
+    }
+    
 }

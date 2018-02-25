8b2ec528076cea07c7339f470f3aaa37c9e7a29f
Adam Borek <adambor3k@gmail.com>
2018-02-25 14:55:28 +0100

    Adds location to Task

--- Offline/DomainModel.swift
+++ Offline/DomainModel.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import CoreLocation
+
+extension CLLocationCoordinate2D {
+    func translate(byLatitude latitudeDelta: Double, byLongitude longitudeDelta: Double) -> CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: self.latitude + latitudeDelta , longitude: self.longitude + longitudeDelta)
+    }
+}
+
+let firaBarcelonaGranVia = CLLocationCoordinate2D(latitude: 41.3544734, longitude: 2.1256795)
+private func randomizeLocation() -> CLLocationCoordinate2D {
+    let latitudeDelta = Double(arc4random_uniform(4001)) * 0.00001
+    let longitudeDelta = Double(arc4random_uniform(5001)) * 0.00001
+    return firaBarcelonaGranVia.translate(byLatitude: latitudeDelta, byLongitude: longitudeDelta)
+}
 
 class Task {
+    let source: MyPrefixSalesOrderHeader
+
     let taskID: String
     let lifeCycleStatusName: String
     let salesOrder: MyPrefixSalesOrderHeader
+    let coordinates: CLLocationCoordinate2D = randomizeLocation()
 
     init(mapping: MyPrefixSalesOrderHeader) {
+        source = mapping
         taskID = mapping.salesOrderID ?? "UNKNOWN"
         lifeCycleStatusName = mapping.lifeCycleStatusName ?? "New"
         salesOrder = mapping
